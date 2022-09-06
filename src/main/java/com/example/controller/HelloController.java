6dd21d5027bbfff7d44866f43bdd6a593bc6a89d
wugunkim <wugunkim@gmail.com>
2022-09-06 19:11:10 +0900

    template 엔진 방식 Api 방식

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,29 @@ public class HelloController {
 	public String helloMvc(@RequestParam( "name")String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello-template"; 
+	}
+	
+	@GetMapping("hello-string")
+	@ResponseBody		//http body(응답) 부분에 이 데이터를 직접 넣어주겠다 
+	public String helloString(@RequestParam("name")String name) {
+		return "hello"+ name; 	//Hello spring  
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody													//responseBody 태그와 객체를 반환하면 기본적으로  Json으로 반환 (기본 셋팅) 
+	public hello helloApi(@RequestParam("name")String name) {
+		hello hello = new hello();
+		hello.setName(name);										//Hello객체  
+		return  hello;						//  was -> helloController -> @responseBody return hello(name:spring) -> HttpMessageConverter ( 객체면 JsonConverter작 ->	요청한 웹에 전 
+	}
+	 
+	static class hello{
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name= name;
+		}
 	}
 }
