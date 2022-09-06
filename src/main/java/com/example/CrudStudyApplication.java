c9c3ce6ac33ccb4b19e42fbb9defadfc7b311978
wugunkim <wugunkim@gmail.com>
2022-09-06 18:34:30 +0900

    spring boot web mvc  thyemleaf , controller /

---
+++ src/main/java/com/example/CrudStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	@GetMapping("hello") //url맵핑 /hello 라고 치면 이 메서드 호출 
+	public String hello(Model model) {
+		model.addAttribute("data", "hello");
+		return "hello"; 	//resources 의 hello.html  호출 
+	}
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam("name")String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template"; 
+	}
+}
